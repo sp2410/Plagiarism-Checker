c669a1e136ed981f62e2b856c80d038801a4fa6b
saurav prakash <sprakash24oct@gmail.com>
2021-01-02 20:47:05 +0530

    First Commit for Plagiarism app

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+
+gem 'sinatra'
+
+group :development do
+  gem 'sinatra-contrib'
+  gem 'pry'
+end
---
+++ server.rb
@@ -0,0 +1,30 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'uri'
+require 'net/http'
+require 'pry'
+
+class PlagChecker < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+end
+
+get '/' do
+  @body = {}
+  erb :index
+end
+
+post '/check' do
+  data = params["content"]
+  url = URI('https://www.prepostseo.com/apis/checkPlag')
+  https = Net::HTTP.new url.host, url.port;
+  https.use_ssl = true
+  request = Net::HTTP::Post.new url
+  form_data = [['key', '75648479537863d29863423c5141ec24'],['data', data]]
+  request.set_form form_data, 'multipart/form-data'
+  response = https.request request
+  @body = JSON response.read_body
+  
+  erb :index, locals: {body: @body}
+end
